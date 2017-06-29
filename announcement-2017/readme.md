0284d65b4d31bcb8a76f20794de39f5ca18e1845
Bumhan Yu <bumhan.yu@gmail.com>
2017-06-29 16:20:52 -0400

    reorganized files into new branch

--- announcement-2017/readme.md
+++
@@ -1,1 +0,0 @@
-A little static site for my new-born baby daughter. Starting with a simple baby announcement, this site will grow into a journal of her journey in this world.
---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+  var B = {
+    init: function() {
+      this.bindEvents();
+      $('.year').text(new Date().getFullYear());
+    },
+    bindEvents: function() {
+      $('div.menu').on('click touch', this.toggleMenu.bind(this));
+      $('a[href^="#"]').on('click touch', this.smoothScroll.bind(this));
+      $('.swipebox' ).swipebox();
+    },
+    toggleMenu: function() {
+      $('body').toggleClass('noscroll');
+      if ($('body').hasClass('noscroll')) {
+        $('nav').fadeIn(100);
+        $('div.menu').addClass('close');
+
+      } else {
+        $('nav').fadeOut(100);
+        $('div.menu').removeClass('close');
+      }
+    },
+    smoothScroll: function (e) {
+      e.preventDefault();
+      this.toggleMenu();
+      var target = e.target.hash;
+      var $target = $(target);
+
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top
+      }, 400, 'swing');
+    }
+  };
+
+  B.init();
+});
